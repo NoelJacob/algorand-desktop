103c625fcc693356373f018776594630ba5e5c92
Noel Jacob <noeljacob91@gmail.com>
2024-04-24 01:47:14 +0530

    Updated JS and Rust deps

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,11 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_store::PluginBuilder;
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri_plugin_store::Builder;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
-        .plugin(PluginBuilder::default().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+      .plugin(Builder::default().build())
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
